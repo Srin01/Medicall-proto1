8d20bbbff7c38d6f132b04eb1201c838cd029f55
Srinidhi Prasad <srinidhiprasad05@gmail.com>
2020-08-06 23:10:40 +0530

    Updates

---
+++ precache-manifest.6dfdf24f79323e59982072d46b680f9a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "422bb6a603719d1b8013772729647cbb",
+    "url": "/Medicall-proto1/index.html"
+  },
+  {
+    "revision": "b4b9b6eb204f779716bc",
+    "url": "/Medicall-proto1/static/css/2.9397f8b9.chunk.css"
+  },
+  {
+    "revision": "a87736ce5cbf40c39a86",
+    "url": "/Medicall-proto1/static/css/main.1a3a4e06.chunk.css"
+  },
+  {
+    "revision": "b4b9b6eb204f779716bc",
+    "url": "/Medicall-proto1/static/js/2.94670ba4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Medicall-proto1/static/js/2.94670ba4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a87736ce5cbf40c39a86",
+    "url": "/Medicall-proto1/static/js/main.0a876894.chunk.js"
+  },
+  {
+    "revision": "147c11466217cee43f96",
+    "url": "/Medicall-proto1/static/js/runtime-main.f6f4ca43.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Medicall-proto1/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Medicall-proto1/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Medicall-proto1/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Medicall-proto1/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Medicall-proto1/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
