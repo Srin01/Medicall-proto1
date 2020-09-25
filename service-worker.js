02064dd86b0d09d1acac8f362e7eaa1b32497732
Srinidhi Prasad <srinidhiprasad05@gmail.com>
2020-09-25 13:58:16 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Medicall-proto1/precache-manifest.6dfdf24f79323e59982072d46b680f9a.js"
+  "/Medicall-proto2/precache-manifest.deb5e9e1f13eb3fd5584e02e33e7bbbe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Medicall-proto1/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Medicall-proto2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
